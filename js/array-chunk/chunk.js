821c5f16e24264d4a18874c7ab5c32f1119d1219
yasin1ar <yasin.amirany@gmail.com>
2024-06-13 22:22:44 +0330

    added chunk.js

---
+++ js/array-chunk/chunk.js
@@ -0,0 +1,35 @@
+// given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+
+function chunk(arr, size) {
+  /**
+   * Splits an array into smaller chunks of a specified size.
+   *
+   * @param {Array} arr - The input array to be split.
+   * @param {number} size - The desired size of each chunk.
+   * @returns {Array} An array of arrays, where each sub-array contains `size` elements.
+   */
+  let d = arr.length / size;
+  //   console.log("init d ", d);
+  let baseArray = [];
+  if (d < 1) {
+    baseArray.push(arr);
+    return baseArray;
+  } else {
+    while (d > 1) {
+      baseArray.push(arr.slice(0, size));
+      //   console.log("base ", baseArray);
+      arr = arr.slice(size);
+      //   console.log("arr ", arr);
+      d = arr.length / size;
+      //   console.log("d ", d);
+    }
+    baseArray.push(arr);
+    return baseArray;
+  }
+}
+
+console.log(chunk([1, 2, 3, 4], 2));
+console.log(chunk([1, 2, 3, 4, 5], 2));
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(chunk([1, 2, 3, 4], 10));
