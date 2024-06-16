902630ff5e3a90d8aa98d1204290f9ddbc7287db
yasin1ar <yasin.amirany@gmail.com>
2024-06-16 18:02:43 +0330

    added chunk.js improved version

---
+++ js/Pyramid/pyramid.js
@@ -0,0 +1,24 @@
+function pyramid(n) {
+  /**
+   * Generates a pyramid pattern with '#' characters.
+   *
+   * @param {number} n - The number of rows in the pyramid.
+   */
+  let mid = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let line = "";
+    for (let column = 0; column <= 2 * n - 1; column++) {
+      if (column >= mid - row && column <= mid + row) {
+        line += "#";
+      } else {
+        line += " ";
+      }
+    }
+    console.log(line);
+  }
+}
+// testing output
+pyramid(2);
+pyramid(3);
+pyramid(9);
+pyramid(20);
