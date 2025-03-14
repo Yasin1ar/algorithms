0bbcf4be10c8639a5d08cf0b2a39c76f79069a1e
yasin1ar <yasin.amirany@gmail.com>
2025-03-14 04:11:51 +0330

    added new

---
+++ JavaScript/leetCode/romanToInteger.js
@@ -0,0 +1,33 @@
+let romanNumerals = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(input = "I") {
+  let decoded = romanNumerals[input[0]];
+  let previousValue = decoded;
+  for (let i = 1; i < input.length; i++) {
+    let currentValue = romanNumerals[input[i]];
+    if (currentValue > previousValue) {
+      if (
+        previousValue * 5 === currentValue ||
+        previousValue * 10 === currentValue
+      ) {
+        decoded = decoded - previousValue + (currentValue - previousValue);
+      } else {
+        return -1;
+      }
+    } else {
+      decoded += currentValue;
+    }
+    previousValue = currentValue;
+  }
+  return decoded;
+}
+
+module.exports = romanToInt;
