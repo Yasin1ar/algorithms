47cd6761a1fdaf49303ef5bc0a6620de3f984874
yasin1ar <yasin.amirany@gmail.com>
2025-04-08 20:20:30 +0330

    (longest_common_prefix.py) add docstring

--- leetCode/longest_common_prefix.py
+++ leetCode/longest_common_prefix.py
@@ -1,17 +1,31 @@
-def longestCommonPrefix(strs: list[str]) -> str:
-    first_item = strs[0]
-    len_first_item = len(first_item)
-    len_strs = len(strs)
-    lcp = ""
-    for i in range(len_first_item):
-        for j in range(1, len_strs):
-            if i >= len(strs[j]):
-                return strs[j]
-            if first_item[i] != strs[j][i]:
-                return lcp
-        lcp += first_item[i]
 
-    return lcp
+def longestCommonPrefix(strs: list[str]) -> str:
+	"""
+	Find the longest common prefix among a list of strings.
+
+	Args:
+		strs: A list of strings to find the common prefix for
+
+	Returns:
+		The longest common prefix as a string
+	"""
+	# Handle edge cases
+	if not strs:
+		return ""
+	
+	first_item = strs[0]
+	len_first_item = len(first_item)
+	len_strs = len(strs)
+	lcp = ""
+	for i in range(len_first_item):
+		for j in range(1, len_strs):
+			if i >= len(strs[j]):
+				return strs[j]
+			if first_item[i] != strs[j][i]:
+				return lcp
+		lcp += first_item[i]
+
+	return lcp
 
 strs = ["fljyyy", "fl", "f", "flivi"]
 print(longestCommonPrefix(strs))
